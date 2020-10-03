e39efa9d043b6b302552a92589413c4c1c779a7a
mosdef71 <krag71@mail.ru>
2020-10-03 16:37:31 +0300

    feat: add functions to search for minimum, maximum and average values

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+      let minNum = Math.min(... array);
+      return minNum;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let maxNum = Math.max(... array);
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+      let summ = 0;
+      for (a of array) {
+        summ += a;
+      }
+      return (summ / array.length);
+  } 
 }
